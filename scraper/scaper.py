bdb91a4f38ccbebf01f2e28cb2f8cb3bc2a98f70
Ahmed Nour Eldeen <ahmednoreldeen@gmail.com>
2024-10-28 23:38:57 +0300

    Implement random user-agent rotation to help in avoiding being blocked.

--- scraper/scaper.py
+++ scraper/scaper.py
@@ -1,3 +1,4 @@
+import random
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 import requests
@@ -21,6 +22,32 @@ def save_products(brand, products):
         product.image_url=p.select_one("img")['src']
         product.save()
 
+ua_pct = {
+    "ua": {
+        "0": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+        "1": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:105.0) Gecko/20100101 Firefox/105.0",
+        "2": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+        "3": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:106.0) Gecko/20100101 Firefox/106.0",
+        "4": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+        "5": "Mozilla/5.0 (X11; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+        "6": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36",
+        "7": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36",
+        "8": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/106.0.0.0 Safari/537.36",
+        "9": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.0 Safari/605.1.15",
+        "10": "Mozilla/5.0 (X11; Linux x86_64; rv:106.0) Gecko/20100101 Firefox/106.0",
+        # "11": "Mozilla/5.0 (Windows NT 10.0; rv:105.0) Gecko/20100101 Firefox/105.0",
+        # "12": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:105.0) Gecko/20100101 Firefox/105.0",
+        # "13": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:105.0) Gecko/20100101 Firefox/105.0",
+        "14": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+    },
+    "pct": {"0": 28.8, "1": 13.28, "2": 10.98, "3": 8.55, "4": 6.25, "5": 5.56, "6": 4.53, "7": 4.27,
+            "8": 3.57, "9": 2.93, "10": 2.99, "14": 1.59,
+            # "11": 2.55, "12": 2.44, "13": 1.7
+            }}
+
+def random_ua():
+    return random.choices(list(ua_pct['ua'].values()), list(ua_pct['pct'].values()))[0]
+
 custom_headers = {
         'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36',
         'accept-language': 'en-GB,en;q=0.9',
@@ -29,12 +56,11 @@ custom_headers = {
 def get_brand_products(brand):
     # search by brand name at first
     url = '{0}s?k={1}'.format(amazon_base_url, brand.name)
-    
 
     response = requests.get(url, headers=custom_headers)    
     print("response 1 status ", response.status_code)
     soup = BeautifulSoup(response.text, 'lxml')
-    # Find the brand filter and get the URL for the filtering by the specfic brand name for more precise result
+    # Find the brand filter and get the URL for the filtering by the specific brand name for more precise result
     filter_href = soup.select_one('#brandsRefinements>ul li[aria-label="{0}" i] a'.format(brand.name))["href"]    
 
     brand_filter_url = urljoin(amazon_base_url, filter_href)
@@ -50,6 +76,9 @@ def get_brand_products(brand):
 
 def parse_page(brand, page_url):    
     print('-------------- start parse ', page_url)
+    # select random user-agent to avoid being blocked
+    custom_headers["user-agent"] = random_ua()
+    print("user-agent ----> ", custom_headers["user-agent"])
     response = requests.get(page_url, headers=custom_headers)    
     print("response status ", response.status_code)
     soup = BeautifulSoup(response.text, 'lxml')
